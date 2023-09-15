5d7411e598ea22432ad4c230a6742db4e67d2f3a
mdalamin3091 <alamin@hypescout.co>
2023-09-15 22:41:48 +0600

    modify sidebar

--- src/layout/Sidebar/index.jsx
+++ src/layout/Sidebar/index.jsx
@@ -1,17 +1,11 @@
 import { Divider, Drawer } from '@mantine/core';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectOpendrawer } from '@/redux/features/sidebar/sidebarSelector';
-import { setOpenDrawer } from '@/redux/features/sidebar/sidebarSlice';
 import SidebarHeader from './SidebarHeader';
 
 const Sidebar = () => {
 
     const openDrawer =  useSelector(selectOpendrawer)
-    // const dispatch = useDispatch();
-
-    // const handleDrawer = () =>{
-    //     dispatch(setOpenDrawer(false))
-    // }
     
     return (
         <>
