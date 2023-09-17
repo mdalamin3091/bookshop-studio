d37ce6105c72098f4e6707ebccf3888b3b29c903
mdalamin3091 <alamin@hypescout.co>
2023-09-18 01:41:10 +0600

    shipping service section dynamic done

--- src/features/home/components/ShippingService.jsx
+++ src/features/home/components/ShippingService.jsx
@@ -1,59 +1,47 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Box, Container, Flex, SimpleGrid, Text, Title } from "@mantine/core";
 import useResponsive from "@/hooks/useResponsive";
+import { selectThemeInfo } from "@/redux/features/themeData/themeSelector";
 import styles from "../index.module.scss";
 
 const ShippingService = () => {
     const { isMobile } = useResponsive();
-
+    const { data: { theme_info: { service_PWt3Ot } = {} } = {} } = useSelector(selectThemeInfo) || {};
     return (
         <Container size="xl" py="xl">
             <SimpleGrid
                 cols={isMobile ? 1 : 3}
                 className={styles.shipping_service_wrap}
             >
-                <Flex
-                    align="center"
-                    justify="center"
-                    className={styles.shipping_service_item}
-                >
-                    <Box>
-                        <Title size="h1" c="#007bff" mb="sm" fz="md">
-                            FREE SHIPPING
-                        </Title>
-                        <Text color="#444444" fz="sm">
-                            Free Shipping World Wide
-                        </Text>
-                    </Box>
-                </Flex>
-                <Flex
-                    align="center"
-                    justify="center"
-                    className={styles.shipping_service_item}
-                >
-                    <Box>
-                        <Title size="h1" c="#007bff" mb="sm" fz="md">
-                            24 X 7 SERVICE
-                        </Title>
-                        <Text color="#444444" fz="sm">
-                            Online Service For New Customer
-                        </Text>
-                    </Box>
-                </Flex>
-                <Flex
-                    align="center"
-                    justify="center"
-                    className={styles.shipping_service_item}
-                >
-                    <Box>
-                        <Title size="h1" c="#007bff" mb="sm" fz="md">
-                            FESTIVAL OFFER
-                        </Title>
-                        <Text color="#444444" fz="sm">
-                            New Online Special Festival Offer
-                        </Text>
-                    </Box>
-                </Flex>
+                {service_PWt3Ot &&
+                    service_PWt3Ot?.list.map((service, index) => (
+                        <Flex
+                            align="center"
+                            justify="center"
+                            className={styles.shipping_service_item}
+                            key={index}
+                        >
+                            <Box>
+                                <Title
+                                    size="h1"
+                                    c="#007bff"
+                                    mb="sm"
+                                    fz="md"
+                                    tt="uppercase"
+                                >
+                                    {service.title}
+                                </Title>
+                                <Text
+                                    color="#444444"
+                                    fz="sm"
+                                    dangerouslySetInnerHTML={{
+                                        __html: service.content
+                                    }}
+                                ></Text>
+                            </Box>
+                        </Flex>
+                    ))}
             </SimpleGrid>
         </Container>
     );
