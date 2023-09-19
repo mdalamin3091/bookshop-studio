fd3cc7f25ff0683123b5d3cf8020ff1e884e95bd
mdalamin3091 <alamin@safewheel.zyz>
2023-09-19 10:04:35 +0600

    fixed reload problem

--- src/features/productDetails/components/TabSection.jsx
+++ src/features/productDetails/components/TabSection.jsx
@@ -2,64 +2,56 @@ import { Carousel } from "@mantine/carousel";
 import { Tabs } from "@mantine/core";
 
 const TabSection = ({ data }) => {
-    return (
-        <Tabs
-            color="primaryColors.0"
-            defaultValue={data.overview ? "overview" : "gallery"}
+  return (
+    <Tabs
+      color="primaryColors.0"
+      defaultValue={data.overview ? "overview" : "gallery"}
+    >
+      <Tabs.List>
+        {data?.overview && (
+          <Tabs.Tab value="overview" color="primaryColors.0" tt="uppercase">
+            Overview
+          </Tabs.Tab>
+        )}
+        {data?.gallery && (
+          <Tabs.Tab value="gallery" color="primaryColors.0" tt="uppercase">
+            Gallery
+          </Tabs.Tab>
+        )}
+      </Tabs.List>
+
+      <Tabs.Panel value="overview" pt="md">
+        {data?.overview}
+      </Tabs.Panel>
+
+      <Tabs.Panel value="gallery" pt="md">
+        <Carousel
+          mx="auto"
+          slideSize="16.666%"
+          withControls={false}
+          slideGap="xl"
+          align="start"
+          slidesToScroll={1}
         >
-            <Tabs.List>
-                {data?.overview && (
-                    <Tabs.Tab
-                        value="overview"
-                        color="primaryColors.0"
-                        tt="uppercase"
-                    >
-                        Overview
-                    </Tabs.Tab>
-                )}
-                {data?.gallery && (
-                    <Tabs.Tab
-                        value="gallery"
-                        color="primaryColors.0"
-                        tt="uppercase"
-                    >
-                        Gallery
-                    </Tabs.Tab>
-                )}
-            </Tabs.List>
-
-            <Tabs.Panel value="overview" pt="md">
-                {data?.overview}
-            </Tabs.Panel>
-
-            <Tabs.Panel value="gallery" pt="md">
-                <Carousel
-                    mx="auto"
-                    slideSize="16.666%"
-                    withControls={false}
-                    slideGap="xl"
-                    align="start"
-                    slidesToScroll={1}
-                >
-                    {data?.gallery &&
-                        data?.gallery.map((item, index) => (
-                            <Carousel.Slide
-                                align="center"
-                                justify="flex-start"
-                                key={data?.id}
-                            >
-                                <img
-                                    src={item.src || data.thumbnail.src}
-                                    alt={item.alt || data.thumbnail.alt}
-                                    key={data?.id}
-                                    style={{width:"100%"}}
-                                />
-                            </Carousel.Slide>
-                        ))}
-                </Carousel>
-            </Tabs.Panel>
-        </Tabs>
-    );
+          {data?.gallery &&
+            data?.gallery.map((item, index) => (
+              <Carousel.Slide
+                align="center"
+                justify="flex-start"
+                key={data?.id}
+              >
+                <img
+                  src={item.src || data.thumbnail.src}
+                  alt={item.alt || data.thumbnail.alt}
+                  key={data?.id}
+                  style={{ width: "100%" }}
+                />
+              </Carousel.Slide>
+            ))}
+        </Carousel>
+      </Tabs.Panel>
+    </Tabs>
+  );
 };
 
 export default TabSection;
--- src/pages/product/[slug].jsx
+++ src/pages/product/[slug].jsx
@@ -1,47 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@mantine/core";
 import DetailsSection from "@/features/productDetails/components/DetailsSection";
 import RelatedProduct from "@/features/productDetails/components/RelatedProduct";
-import { getProductDetails, useGetProductDetailsQuery } from "@/redux/features/products/productApi";
+import {
+  getProductDetails,
+  useGetProductDetailsQuery,
+} from "@/redux/features/products/productApi";
 import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { wrapper } from "@/redux/app/store";
 import { useRouter } from "next/router";
 import { getRunningQueriesThunk } from "@/redux/app/api/apiSlice";
+import {
+  getThemeData,
+  useGetThemeDataQuery,
+} from "@/redux/features/themeData/themeDataApi";
+import { useDispatch } from "react-redux";
+import { setThemeInfo } from "@/redux/features/themeData/themeDataSlice";
 
 const ProductDetails = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const { data: themData, isSuccess } = useGetThemeDataQuery(undefined);
+  const dispatch = useDispatch();
 
-    const slug = router.query.slug;
-    const result = useGetProductDetailsQuery(
-        typeof slug === "string" ? slug : skipToken,
-        {
-            // If the page is not yet generated, router.isFallback will be true
-            // initially until getStaticProps() finishes running
-            skip: router.isFallback,
-        }
-    );
-    const { isLoading, error, data } = result;
-    console.log("result", data?.data)
-    return (
-        <Container size="xl" py="50px">
-            <DetailsSection data={data?.data}/>
-            {/* <RelatedProduct /> */}
-        </Container>
-    );
+  useEffect(() => {
+    if (isSuccess && themData) {
+      dispatch(setThemeInfo(themData));
+    }
+  }, [themData, dispatch, isSuccess]);
+
+  const slug = router.query.slug;
+  const result = useGetProductDetailsQuery(
+    typeof slug === "string" ? slug : skipToken,
+    {
+      // If the page is not yet generated, router.isFallback will be true
+      // initially until getStaticProps() finishes running
+      skip: router.isFallback,
+    }
+  );
+  const { isLoading, error, data } = result;
+  console.log("result", data?.data);
+  return (
+    <Container size="xl" py="50px">
+      <DetailsSection data={data?.data} />
+      {/* <RelatedProduct /> */}
+    </Container>
+  );
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
-    (store) => async (context) => {
-        const slug = context.params?.slug;
-        if (typeof slug === "string") {
-            store.dispatch(getProductDetails.initiate(slug));
-        }
-
-        await Promise.all(store.dispatch(getRunningQueriesThunk()));
-        return {
-            props: {},
-        };
+  (store) => async (context) => {
+    const slug = context.params?.slug;
+    if (typeof slug === "string") {
+      store.dispatch(getProductDetails.initiate(slug));
     }
+    store.dispatch(getThemeData.initiate());
+
+    await Promise.all(store.dispatch(getRunningQueriesThunk()));
+    return {
+      props: {},
+    };
+  }
 );
 
 export default ProductDetails;
