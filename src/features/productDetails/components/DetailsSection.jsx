6e0b423b2e98460f4f2869bffa57f79df1bae05a
mdalamin3091 <alamin@safewheel.zyz>
2023-09-19 10:06:33 +0600

    fixed details page problem

--- src/features/productDetails/components/DetailsSection.jsx
+++ src/features/productDetails/components/DetailsSection.jsx
@@ -1,21 +1,21 @@
 import React, { Fragment } from "react";
 import {
-    Box,
-    Button,
-    Divider,
-    Flex,
-    Grid,
-    Rating,
-    Stack,
-    Text,
-    Title,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Grid,
+  Rating,
+  Stack,
+  Text,
+  Title,
 } from "@mantine/core";
 import {
-    BiLogoFacebook,
-    BiLogoLinkedinSquare,
-    BiLogoPinterestAlt,
-    BiLogoTwitter,
-    BiLogoWhatsapp,
+  BiLogoFacebook,
+  BiLogoLinkedinSquare,
+  BiLogoPinterestAlt,
+  BiLogoTwitter,
+  BiLogoWhatsapp,
 } from "react-icons/bi";
 import useResponsive from "@/hooks/useResponsive";
 import QuantityInput from "./QuantityInput";
@@ -24,32 +24,32 @@ import { Carousel } from "@mantine/carousel";
 import TabSection from "./TabSection";
 
 const DetailsSection = ({ data }) => {
-    const { isMobile } = useResponsive();
-    return (
-        <Fragment>
-            <Grid gutter="xl">
-                <Grid.Col span={isMobile ? 12 : 4}>
-                    <Box className={styles.thumbnail}>
-                        <Carousel
-                            mx="auto"
-                            slideSize="100%"
-                            withControls={true}
-                            slideGap="xl"
-                            align="start"
-                            slidesToScroll={1}
-                        >
-                            {data?.gallery &&
-                                data?.gallery.map((item, index) => (
-                                    <Carousel.Slide key={data?.id}>
-                                        <img
-                                            src={item.src || data.thumbnail.src}
-                                            alt={item.alt || data.thumbnail.alt}
-                                        />
-                                    </Carousel.Slide>
-                                ))}
-                        </Carousel>
-                    </Box>
-                    {/* <Carousel
+  const { isMobile } = useResponsive();
+  return (
+    <Fragment>
+      <Grid gutter="xl">
+        <Grid.Col span={isMobile ? 12 : 4}>
+          <Box className={styles.thumbnail}>
+            <Carousel
+              mx="auto"
+              slideSize="100%"
+              withControls={true}
+              slideGap="xl"
+              align="start"
+              slidesToScroll={1}
+            >
+              {data?.gallery &&
+                data?.gallery.map((item, index) => (
+                  <Carousel.Slide key={data?.id}>
+                    <img
+                      src={item.src || data.thumbnail.src}
+                      alt={item.alt || data.thumbnail.alt}
+                    />
+                  </Carousel.Slide>
+                ))}
+            </Carousel>
+          </Box>
+          {/* <Carousel
                         mx="auto"
                         slideSize="33.333%"
                         withControls={false}
@@ -72,94 +72,96 @@ const DetailsSection = ({ data }) => {
                                 </Carousel.Slide>
                             ))}
                     </Carousel> */}
-                </Grid.Col>
-                <Grid.Col span={isMobile ? 12 : 5}>
-                    <Stack spacing="md">
-                        <Title size="h2" fz="25px" c="#222222">
-                            {data?.name}
-                        </Title>
-                        <Rating readOnly value={data?.rating?.avg} />
+        </Grid.Col>
+        <Grid.Col span={isMobile ? 12 : 5}>
+          <Stack spacing="md">
+            <Title size="h2" fz="25px" c="#222222">
+              {data?.name}
+            </Title>
+            <Rating readOnly value={data?.rating?.avg} />
 
-                        <Box>
-                            <Text fz="md" c="#212529">
-                                Price: ৳{data?.price.max}
-                            </Text>
-                            <Divider variant="dotted" />
-                        </Box>
-                        <Text c="#00e600" fw="bold" fz="sm">
-                            In Stock
-                        </Text>
-                        <Box>
-                            <Text fz="sm" fw="bold" mb="sm">
-                                Quantity
-                            </Text>
-                            <QuantityInput />
-                        </Box>
-                        <Box>
-                            <Box fz="sm">
-                                <Text fz="sm">Total Price:</Text>
-                                <Text span fz="lg">
-                                    ৳
-                                </Text>
-                                <Text span fz="24px">
-                                    {data.price.base_sale}
-                                </Text>
-                            </Box>
-                        </Box>
-                        <Flex gap="md">
-                            <Button
-                                uppercase
-                                color="primaryColors.0"
-                                variant="filled"
-                                radius={"0"}
-                                size="md"
-                            >
-                                Add to cart
-                            </Button>
+            <Box>
+              <Text fz="md" c="#212529">
+                Price: ৳{data?.price.max}
+              </Text>
+              <Divider variant="dotted" />
+            </Box>
+            <Text c="#00e600" fw="bold" fz="sm">
+              In Stock
+            </Text>
+            <Box>
+              <Text fz="sm" fw="bold" mb="sm">
+                Quantity
+              </Text>
+              <QuantityInput />
+            </Box>
+            <Box>
+              <Box fz="sm">
+                <Text fz="sm">Total Price:</Text>
+                <Text span fz="lg">
+                  ৳
+                </Text>
+                <Text span fz="24px">
+                  {data.price.base_sale}
+                </Text>
+              </Box>
+            </Box>
+            <Flex gap="md">
+              <Button
+                uppercase
+                color="primaryColors.0"
+                variant="filled"
+                radius={"0"}
+                size="md"
+              >
+                Add to cart
+              </Button>
 
-                            <Button
-                                uppercase
-                                color="primaryColors.0"
-                                variant="filled"
-                                radius={"0"}
-                                size="md"
-                            >
-                                Buy Now
-                            </Button>
-                        </Flex>
-                        <Divider variant="dotted" />
-                        {data?.description && (
-                            <Fragment>
-                                <Box>
-                                    <Text fz="sm" fw="bold">
-                                        Product Details
-                                    </Text>
-                                    <Text fz="14px" c="#212529">
-                                        {data?.description}
-                                    </Text>
-                                </Box>
-                                <Divider variant="dotted" />
-                            </Fragment>
-                        )}
-                        <Box>
-                            <Text fz="sm" fw="bold" mb="sm">
-                                Share It
-                            </Text>
-                            <Flex gap="lg">
-                                <BiLogoFacebook />
-                                <BiLogoPinterestAlt />
-                                <BiLogoTwitter />
-                                <BiLogoWhatsapp />
-                                <BiLogoLinkedinSquare />
-                            </Flex>
-                        </Box>
-                    </Stack>
-                </Grid.Col>
-                {isMobile || <Grid.Col span={3}></Grid.Col>}
-            </Grid>
-            <TabSection data={data}/>
-        </Fragment>
-    );
+              <Button
+                uppercase
+                color="primaryColors.0"
+                variant="filled"
+                radius={"0"}
+                size="md"
+              >
+                Buy Now
+              </Button>
+            </Flex>
+            <Divider variant="dotted" />
+            {data?.description && (
+              <Fragment>
+                <Box>
+                  <Text fz="sm" fw="bold">
+                    Product Details
+                  </Text>
+                  <Text
+                    fz="14px"
+                    c="#212529"
+                    dangerouslySetInnerHTML={{ __html: data?.description }}
+                  />
+                </Box>
+                <Divider variant="dotted" />
+              </Fragment>
+            )}
+            <Box>
+              <Text fz="sm" fw="bold" mb="sm">
+                Share It
+              </Text>
+              <Flex gap="lg">
+                <BiLogoFacebook />
+                <BiLogoPinterestAlt />
+                <BiLogoTwitter />
+                <BiLogoWhatsapp />
+                <BiLogoLinkedinSquare />
+              </Flex>
+            </Box>
+          </Stack>
+        </Grid.Col>
+        {isMobile || <Grid.Col span={3}></Grid.Col>}
+      </Grid>
+      <TabSection data={data} />
+    </Fragment>
+  );
 };
 
 export default DetailsSection;
--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {
-    Button,
-    Container,
-    Flex,
-    Grid,
-    Input,
-    Stack,
-    Text,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  Input,
+  Stack,
+  Text,
 } from "@mantine/core";
 import Image from "next/image";
 import { HiMapPin, HiOutlineEnvelope } from "react-icons/hi2";
@@ -18,98 +18,83 @@ import { useSelector } from "react-redux";
 import { selectThemeInfo } from "@/redux/features/themeData/themeSelector";
 
 const Footer = () => {
-    const { isMobile } = useResponsive();
-    const { data: { footer: { body } = {} } = {} } =
-        useSelector(selectThemeInfo) || {};
-    const { column1, column2, column3, column4 } = body || {};
-    return (
-        <Container size="xl" py="xl">
-            <Grid>
-                {/* col - 1 */}
-                <Grid.Col span={isMobile ? 12 : 5}>
-                    {column1?.list.logo_4ECq53.allow && (
-                        <Image
-                            src={"https://i.ibb.co/YZ1SDCR/logo.jpg"}
-                            width={250}
-                            height={65}
-                            objectFit="cover"
-                            alt="logo"
-                            priority
-                        />
-                    )}
-                    <Text
-                        c="#444444"
-                        lh="32px"
-                        mt="25px"
-                        pr="50px"
-                        mb="md"
-                        dangerouslySetInnerHTML={{
-                            __html: column1?.list?.text_oaT6SY.content,
-                        }}
-                    ></Text>
-                </Grid.Col>
+  const { isMobile } = useResponsive();
+  const { data: { footer: { body } = {} } = {} } =
+    useSelector(selectThemeInfo) || {};
+  const { column1, column2, column3, column4 } = body || {};
+  return (
+    <Container size="xl" py="xl">
+      <Grid>
+        {/* col - 1 */}
+        <Grid.Col span={isMobile ? 12 : 5}>
+          {column1?.list.logo_4ECq53.allow && (
+            <Image
+              src={"https://i.ibb.co/YZ1SDCR/logo.jpg"}
+              width={250}
+              height={65}
+              objectFit="cover"
+              alt="logo"
+              priority
+            />
+          )}
+          <Text
+            c="#444444"
+            lh="32px"
+            mt="25px"
+            pr="50px"
+            mb="md"
+            dangerouslySetInnerHTML={{
+              __html: column1?.list?.text_oaT6SY.content,
+            }}
+          ></Text>
+        </Grid.Col>
 
-                {/* col - 2 */}
-                <Grid.Col span={isMobile ? 12 : 3}>
-                    <Text fz="36px" c="#222222" fw="bold" lh="37px" mb="5px">
-                        {column2?.list?.newsletter_nvABLM?.title}
-                    </Text>
-                    <Input placeholder="Enter your email" mb="lg" />
+        {/* col - 2 */}
+        <Grid.Col span={isMobile ? 12 : 3}>
+          <Text fz="36px" c="#222222" fw="bold" lh="37px" mb="5px">
+            {column2?.list?.newsletter_nvABLM?.title}
+          </Text>
+          <Input placeholder="Enter your email" mb="lg" />
 
-                    <Button uppercase color="primaryColors.0" variant="filled">
-                        Subscribe
-                    </Button>
-                </Grid.Col>
+          <Button uppercase color="primaryColors.0" variant="filled">
+            Subscribe
+          </Button>
+        </Grid.Col>
 
-                {/* col - 3 */}
-                <Grid.Col span={isMobile ? 12 : 2}>
-                    <Stack mt="lg" gap="md">
-                        <Flex
-                            align="center"
-                            justify="flex-start"
-                            gap="sm"
-                            c="#999999"
-                        >
-                            <HiMapPin />
-                            <Text fz="sm">Dhaka, Bangladesh</Text>
-                        </Flex>
-                        <Flex
-                            align="center"
-                            justify="flex-start"
-                            gap="sm"
-                            c="#999999"
-                        >
-                            <IoMdCall />
-                            <Text fz="sm">01700705293</Text>
-                        </Flex>
-                        <Flex
-                            align="center"
-                            justify="flex-start"
-                            gap="sm"
-                            c="#999999"
-                        >
-                            <HiOutlineEnvelope />
-                            <Text fz="sm">info@webmanza.com</Text>
-                        </Flex>
-                    </Stack>
-                </Grid.Col>
+        {/* col - 3 */}
+        <Grid.Col span={isMobile ? 12 : 2}>
+          <Stack mt="lg" gap="md">
+            <Flex align="center" justify="flex-start" gap="sm" c="#999999">
+              <HiMapPin />
+              <Text fz="sm">Dhaka, Bangladesh</Text>
+            </Flex>
+            <Flex align="center" justify="flex-start" gap="sm" c="#999999">
+              <IoMdCall />
+              <Text fz="sm">01700705293</Text>
+            </Flex>
+            <Flex align="center" justify="flex-start" gap="sm" c="#999999">
+              <HiOutlineEnvelope />
+              <Text fz="sm">info@webmanza.com</Text>
+            </Flex>
+          </Stack>
+        </Grid.Col>
 
-                {/* col - 4 */}
-                <Grid.Col span={isMobile ? 12 : 2}>
-                    <Text c="#222222" fz="18px" fw="bold" mb="lg" tt="uppercase">
-                        {column4?.title}
-                    </Text>
-                    <Stack gap="sm">
-                        {
-                            column4?.list?.menubar_87srO8?.list.map((link) =>(
-                                <Link href={link.path} key={link.id}>{link.text}</Link>
-                            ))
-                        }
-                    </Stack>
-                </Grid.Col>
-            </Grid>
-        </Container>
-    );
+        {/* col - 4 */}
+        <Grid.Col span={isMobile ? 12 : 2}>
+          <Text c="#222222" fz="18px" fw="bold" mb="lg" tt="uppercase">
+            {column4?.title}
+          </Text>
+          <Stack gap="sm">
+            {column4?.list?.menubar_87srO8?.list.map((link) => (
+              <Link href={link.path} key={link.id}>
+                {link.text}
+              </Link>
+            ))}
+          </Stack>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Footer;
