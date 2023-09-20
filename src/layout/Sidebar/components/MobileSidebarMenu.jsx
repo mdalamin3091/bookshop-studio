41b7446987b4e2a983ee63b5f1c781323d58fa8f
mdalamin3091 <alamin@safewheel.zyz>
2023-09-20 17:49:48 +0600

    mobile nested link done

--- src/layout/Sidebar/components/MobileSidebarMenu.jsx
+++ src/layout/Sidebar/components/MobileSidebarMenu.jsx
@@ -1,14 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { useSelector } from "react-redux";
 import React, { Fragment, useState } from "react";
-import { Center, Flex, NavLink, SegmentedControl } from "@mantine/core";
+import { Center, NavLink, SegmentedControl } from "@mantine/core";
 import { selectThemeInfo } from "@/redux/features/themeData/themeSelector";
+
+function createNestedMenu(data, parentId = 0) {
+  const menu = [];
+  for (const item of data) {
+    if (item.parent === parentId) {
+      const subMenu = createNestedMenu(data, item.id);
+      const menuItem = { ...item }; // Create a new object
+      if (subMenu.length > 0) {
+        menuItem.children = subMenu;
+      }
+      menu.push(menuItem);
+    }
+  }
+  return menu;
+}
+
+// Recursive function to render nested links
+function renderNestedLinks(items, depth = 1) {
+  if (!items || items.length === 0 || depth > 3) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <NavLink label={item.text} key={item.id}>
+          {renderNestedLinks(item.children, depth + 1)}
+        </NavLink>
+      ))}
+    </ul>
+  );
+}
 
 const MobileSidebarMenu = () => {
   const [selectMenu, setSelectMenu] = useState("All Category");
   const { data: { header: { body } = {} } = {} } =
     useSelector(selectThemeInfo) || {};
   const { category_menu, primary_menu } = body || {};
+
+  const nestedMenu = createNestedMenu(category_menu);
   return (
     <Fragment>
       <Center mb="lg">
@@ -24,12 +59,7 @@ const MobileSidebarMenu = () => {
 
       {selectMenu === "All Category" &&
         category_menu &&
-        category_menu.map((menu) => (
-          <Flex key={menu.id} align="center" justify="flex-start">
-            <img width="20px" src={menu.icon} alt={menu.text} />
-            <NavLink label={menu.text}></NavLink>
-          </Flex>
-        ))}
+        renderNestedLinks(nestedMenu)}
 
       {selectMenu === "Main Menu" &&
         primary_menu &&
